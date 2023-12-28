a0b7238ff149622dac15094d289e2f42c19e51db
Manu <manuchimsoazubuike@gmail.com>
2023-12-28 15:05:51 +0100

    more tests

--- app.js
+++ app.js
@@ -4,21 +4,23 @@ const app = express()
 app.get('/', (req, res) => {
     res.status(404)
     res.send('wooop')
+    console.log("cweknc");
     res.end()
 })
 app.get('/gitinfo/', (req, res) => {
     res.status(404)
     res.send('wooop')
+    console.log("cewnec");
     res.end()
 })
 
 app.listen(443, () => (
-    console.log('wooop')
+    console.log('woooddddp')
 ))
 app.listen(3000, () => (
-    console.log('wooop')
+    console.log('wooowwwep')
 ))
 
 app.listen(80, () => (
-    console.log('wooop')
+    console.log('wooeeeeop')
 ))
