259007bcfb66ef9e4966ccac191915af2472ecad
Manu <manuchimsoazubuike@gmail.com>
2023-12-28 14:56:13 +0100

    test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,23 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
+    res.status(404)
     res.send('wooop')
+    res.end()
+})
+app.get('/gitinfo/', (req, res) => {
+    res.status(404)
+    res.send('wooop')
+    res.end()
 })
 
 app.listen(443, () => (
+    console.log('wooop')
+))
+app.listen(3000, () => (
+    console.log('wooop')
+))
+
+app.listen(80, () => (
     console.log('wooop')
 ))
